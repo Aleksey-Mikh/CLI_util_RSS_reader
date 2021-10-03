35f8c0ecd464c998424bbe79bb457df52a998702
Aleksey-Mikh <Lehado67@gamil.com>
2021-10-03 17:02:54 +0300

    parsing lenta.ru and add encoding when file is writting

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = "https://news.yahoo.com/rss/"
+URL = "https://lenta.ru/rss"
 HEADERS = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                   "(KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
@@ -11,8 +11,15 @@ HEADERS = {
 
 
 def save_in_file(data, path):
-    with open(path, "w") as file:
+    with open(path, "w", encoding="utf-8") as file:
         file.writelines(data)
+
+
+def read_file(path):
+    with open(path) as file:
+        data = file.read()
+
+    return data
 
 
 def get_html(url):
@@ -20,10 +27,18 @@ def get_html(url):
     return response
 
 
+def get_data(data):
+    pass
+
+
 def parse():
     html = get_html(URL)
     if html.status_code == 200:
-        save_in_file(html.text, "rss.xml")
+        save_in_file(html.text, "lenta_rss.xml")
+        # data = read_file("rss.xml")
+
+    else:
+        print("Error")
 
 parse()
 
