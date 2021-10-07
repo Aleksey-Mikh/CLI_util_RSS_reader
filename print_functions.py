5272a8f5af4123055c000878cc6904d7f50cc33e
Aleksey-Mikh <Lehado67@gamil.com>
2021-10-07 20:32:45 +0300

    add file print_functions which contain functions which will call when programm will need to print some information in console

---
+++ print_functions.py
@@ -0,0 +1,10 @@
+def info_print(message):
+    print(f"[INFO] {message}")
+
+
+def warning_print(message):
+    print(f"[WARNING] {message}")
+
+
+def error_print(message):
+    print(f"[ERROR] {message}")
