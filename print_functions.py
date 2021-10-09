121137c9a4629bacf7bbd037152c9d3131f611f9
Aleksey-Mikh <Lehado67@gamil.com>
2021-10-09 18:05:44 +0300

    add description to function in print_function.py

--- print_functions.py
+++ print_functions.py
@@ -1,10 +1,25 @@
 def info_print(message):
+    """
+    Function which print information message
+
+    :param message: message
+    """
     print(f"[INFO] {message}", end="\n\n")
 
 
 def warning_print(message):
+    """
+    Function which print warning message
+
+    :param message: message
+    """
     print(f"[WARNING] {message}", end="\n\n")
 
 
 def error_print(message):
+    """
+    Function which print error message
+
+    :param message: message
+    """
     print(f"[ERROR] {message}", end="\n\n")
