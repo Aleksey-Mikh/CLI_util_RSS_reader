ee6eb8bbd91cfbab1f56761dc2ac4047e1cb1afb
Aleksey-Mikh <Lehado67@gamil.com>
2021-10-22 12:44:42 +0300

    refactoring conversion_to_html.py, added the ability to create a path

--- Aleksey_Mikhalkevich/cool_project/conversion_to_format/conversion_to_html.py
+++ Aleksey_Mikhalkevich/cool_project/conversion_to_format/conversion_to_html.py
@@ -1,19 +1,23 @@
-import os
-import sys
 from pathlib import Path
 
-from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader
+from jinja2 import Environment, select_autoescape, FileSystemLoader
 
-# from temp import LIST
+from cool_project.cervices.print_functions import error_print, info_print
+
+FILE_NAME = "feed.html"
 
 
+def make_dir(path):
+    if not Path(path).exists():
+        p = Path(path)
+        p.mkdir(parents=True)
 
 
 def is_list(obj):
     return isinstance(obj, list)
 
 
-def convert_to_html(data, path):
+def get_env():
     path = Path(__file__).parent
     path = Path(path, "templates")
     env = Environment(
@@ -22,13 +26,34 @@ def convert_to_html(data, path):
         trim_blocks=True,
         lstrip_blocks=True,
     )
+    return env
 
-    content = {}
-    content["title"] = "Feeds"
-    content["feeds"] = [data]
-    env.tests['is_list'] = is_list
 
+def get_content(data, env):
+    if not is_list(data[0]):
+        data = [data]
+    content = {
+        "title": "Feeds",
+        "feeds": data
+    }
+    env.tests["is_list"] = is_list
+    return content
+
+
+def convert_to_html(data, path, verbose):
+    if verbose:
+        info_print("Conversion to HTML started")
+
+    env = get_env()
+    content = get_content(data, env)
     template = env.get_template("template.html")
     result = template.render(content)
-    with open('temp.html', 'w', encoding="utf-8") as file:
-        file.write(result)
+    path = Path(path)
+    try:
+        make_dir(path)
+        with open(Path(path, FILE_NAME), "w", encoding="utf-8") as file:
+            file.write(result)
+        if verbose:
+            info_print("Conversion to HTML ended")
+    except Exception:
+        error_print("The entered path cannot be created")
