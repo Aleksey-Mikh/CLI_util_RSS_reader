2a5e958e1fd822c93b538d639bb157679c470a32
Aleksey-Mikh <Lehado67@gamil.com>
2021-10-24 00:31:08 +0300

    added test for convert_to_html.py

---
+++ Aleksey_Mikhalkevich/tests/tests_conversion_to_format/test_conversion_to_html.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+
+import pytest
+
+from cool_project.conversion_to_format.conversion_to_html import (
+    make_dir,
+    is_list,
+    get_env,
+    get_content,
+    convert_to_html
+)
+
+
+@pytest.fixture()
+def get_dir():
+    """A fixture that creates a directory and deletes it"""
+    path = Path(Path(__file__).parent, "test_dir")
+    make_dir(path)
+    yield Path.exists(path)
+    Path.rmdir(path)
+
+
+@pytest.fixture()
+def get_file():
+    """A fixture that deletes file"""
+    path = Path(Path(__file__).parent, "feed.html")
+    yield
+    Path.unlink(path)
+
+
+def test_make_dir(get_dir):
+    """test for make_dir"""
+    actual = get_dir
+    assert actual
+
+
+@pytest.mark.parametrize("obj, correct_res",
+                         [("soup_fix", False),
+                          ([1, 2], True)]
+                         )
+def test_is_list(obj, correct_res):
+    """test for is_list"""
+    assert is_list(obj) == correct_res
+
+
+def test_get_env():
+    """test for get_env"""
+    assert get_env()
+
+
+@pytest.mark.parametrize("data, correct_res",
+                         [("data", ["data"]),
+                          ([["data"]], [["data"]])]
+                         )
+def test_get_content(data, correct_res):
+    """test for get_content"""
+    content = get_content(data, get_env())
+    expected = {
+        "title": "Feeds",
+        "feeds": correct_res
+    }
+    assert content == expected
+
+
+def test_convert_to_html(get_file, capsys):
+    """test for convert_to_htm"""
+    data = "data"
+    path = Path(__file__).parent
+    convert_to_html(data, path, True)
+    captured = capsys.readouterr()
+    assert captured.out == f"[INFO] Conversion to HTML started\n\n" \
+                           f"[INFO] Conversion to HTML ended\n\n" \
+                           f"[INFO] A feed in HTML format was saved on the path: " \
+                           f"{Path(path, 'feed.html')}\n\n"
+
+    data = "data"
+    path = Path(Path(__file__).parent, "da:ta")
+    convert_to_html(data, path, True)
+    captured = capsys.readouterr()
+    assert captured.out == f"[INFO] Conversion to HTML started\n\n" \
+                           f"[INFO] Conversion to HTML ended\n\n" \
+                           f"[ERROR] The entered path cannot be created\n\n"
