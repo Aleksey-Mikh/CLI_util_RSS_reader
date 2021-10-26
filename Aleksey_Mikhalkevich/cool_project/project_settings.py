aa65054a1e147eb20a8a7cbe1b6f3e4f1e58510f
Aleksey-Mikh <Lehado67@gamil.com>
2021-10-26 19:01:52 +0300

    finished refactor with pycodestyle

--- Aleksey_Mikhalkevich/cool_project/project_settings.py
+++ Aleksey_Mikhalkevich/cool_project/project_settings.py
@@ -1,8 +1,9 @@
 PROGRAM_VERSION = "4.5.0"
 
 HEADERS = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                  "(KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36",
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/94.0.4606.61 Safari/537.36",
     "accept": "*/*",
     "Content-Type": "charset=UTF-8"
 }
